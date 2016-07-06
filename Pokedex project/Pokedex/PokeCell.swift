569d175768e7f08042a104525eec5a6568cafdf2
bgyorev <bgyorev@itlite.onmicrosoft.com>
2016-07-06 16:26:44 +0300

    Implemented the custom cell class

--- Pokedex project/Pokedex/PokeCell.swift
+++ Pokedex project/Pokedex/PokeCell.swift
@@ -10,4 +10,25 @@ import UIKit
 
 class PokeCell: UICollectionViewCell {
     
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        self.nameLabel.text = pokemon.name.capitalizedString
+        self.thumbImg.image = UIImage(named: "\(self.pokemon.pokedexID)")
+    }
+    
 }
