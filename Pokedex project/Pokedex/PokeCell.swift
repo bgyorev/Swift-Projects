50d0d352bd320ddd7fc702240f1fa6c8afa4e011
bgyorev <bgyorev@itlite.onmicrosoft.com>
2016-07-06 16:26:16 +0300

    Added background image for the main view as well as custom class for the collection view cells

---
+++ Pokedex project/Pokedex/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Bozhidar Gyorev on 7/6/16.
+//  Copyright © 2016 GlaxoSmithKline. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
